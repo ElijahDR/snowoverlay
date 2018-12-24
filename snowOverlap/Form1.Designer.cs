c73b2ffea8df2930e2e1462cf86972f3c0923b73
Elijah Durston-Rand <elijah@elijahdr.com>
2018-12-24 14:44:53 +0000

    Initial

---
+++ snowOverlap/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace snowOverlap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.canvas = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.Color.Transparent;
+            this.canvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(830, 493);
+            this.canvas.TabIndex = 0;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 20;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(830, 493);
+            this.Controls.Add(this.canvas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ snowOverlap/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace snowOverlap
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
+        static extern int GetWindowLongPtr(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong")]
+        private static extern int SetWindowLong32(IntPtr hWnd, int nIndex, int dwNewLong);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+        public int[,] points = new int[150, 5];
+        int maxV = 5;
+        int maxW = 10;
+        Bitmap bmp;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            this.TransparencyKey = Color.Turquoise;
+            this.BackColor = Color.Turquoise;
+            int initialStyle = GetWindowLongPtr(this.Handle, -20);
+            SetWindowLong32(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+            //SetWindowLong(this.Handle, -20, initialStyle | 0x80000 | 0x20);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+            SetForegroundWindow(this.Handle);
+            typeof(Panel).InvokeMember("DoubleBuffered",
+            BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+            null, canvas, new object[] { true });
+            Random rnd = new Random();
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                points[i, 0] = rnd.Next(1, canvas.Width);
+                points[i, 1] = rnd.Next(1, canvas.Height);
+                points[i, 2] = rnd.Next(1, maxV);
+                points[i, 3] = rnd.Next(1, maxV);
+                points[i, 4] = rnd.Next(1, maxW);
+            }
+
+        }
+
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                e.Graphics.FillEllipse(Brushes.White, points[i, 0], points[i, 1], points[i, 4], points[i, 4]);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            updateSnow();
+        }
+
+        public void updateSnow()
+        {
+            for (int i = 0; i < points.GetLength(0); i++)
+            {
+                points[i, 0] += points[i, 2];
+                points[i, 1] += points[i, 3];
+                if (points[i, 0] > canvas.Width)
+                {
+                    points[i, 0] = 0;
+                }
+                if (points[i, 1] > canvas.Height)
+                {
+                    points[i, 1] = 0;
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //canvas.BackgroundImage = bmp;
+            this.Invalidate();
+        }
+    }
+}
